3b3837432c7b1d450b9024e0e6cb35b490b0b501
Mohamed Montasser <mohamed.montasser0385@gmail.com>
2022-03-28 15:22:44 +0200

    finished asset portfolio styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './app.css';
+import Buldings from './components/Buldings';
+import Canvas from './components/Canvas';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <h1>REAL ESTATE <span>TYCOON</span></h1>
+      <hr />
+      <p><small>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis et ipsam ad suscipit iure dolore necessitatibus impedit, voluptatibus vitae ratione asperiores corporis reiciendis mollitia, rerum quos maxime molestias voluptatum tenetur. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem dicta doloremque tempore consectetur cumque, at alias, labore iure nihil soluta debitis unde dolorum sequi error, adipisci molestiae ullam minus aut.</small></p>
       </header>
+      <div className="game">
+        <Portfolio/>
+        <Canvas/>
+        <Buldings/>
+      </div>
     </div>
   );
 }
