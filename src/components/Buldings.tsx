62c57b0a70c969fec90818f6475e522a12ab1b13
Mohamed Montasser <mohamed.montasser0385@gmail.com>
2022-03-28 20:25:25 +0200

    added scrollbar styles

--- src/components/Buldings.tsx
+++ src/components/Buldings.tsx
@@ -5,7 +5,17 @@ function Buldings() {
   return (
     <div className='buildings'>
         <h2>REAL ESTATE AVAILABLE FOR PERCHASE</h2>
-        <div className="buildings__list">
+        <div className="buildings__list scrl">
+            <BuildingItem/>
+            <BuildingItem/>
+            <BuildingItem/>
+            <BuildingItem/>
+            <BuildingItem/>
+            <BuildingItem/>
+            <BuildingItem/>
+            <BuildingItem/>
+            <BuildingItem/>
+            <BuildingItem/>
             <BuildingItem/>
             <BuildingItem/>
         </div>
